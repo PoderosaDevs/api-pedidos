eee7c06859118908d550403e8735a2f2ebcf2cda
Poderosa Beleza <developer@poderosabeleza.com.br>
2025-11-18 08:20:26 -0300

    Update clienteRoutes.ts

--- src/routes/clienteRoutes.ts
+++ src/routes/clienteRoutes.ts
@@ -5,12 +5,8 @@ import {
   atualizarCliente,
   deletarCliente,
 } from "../controllers/clienteController";
-import { autenticar } from "../middlewares/auth";
 
 const router = Router();
-
-// protege todas as rotas abaixo
-router.use(autenticar);
 
 router.get("/", listarClientes);
 router.post("/register", criarCliente);
